2c0453ef1864b2fe5324981db18768dacb8622fc
Calvin Scott <cal@rets.ly>
2016-08-09 14:24:08 -0700

    Initial Commit

---
+++ retsly-swift-sdk/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  retsly-swift-sdk
+//
+//  Created by Calvin Scott on 2016-08-09.
+//  Copyright © 2016 Calvin Scott. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
